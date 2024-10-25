9f16e4ae0ef40601568f4332ef442b1c6043b189
Yurii Vasilenko <anna.busaga@gmail.com>
2024-10-25 13:54:28 +0300

    add docs api & swagger

---
+++ src/middlewares/swaggerDocs.js
@@ -0,0 +1,16 @@
+// src/middlewares/swaggerDocs.js
+
+import fs from 'node:fs';
+import { SWAGGER_PATH } from '../constants/index.js';
+import swaggerUI from 'swagger-ui-express';
+import createHttpError from 'http-errors';
+
+export const swaggerDocs = () => {
+  try {
+    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
+    return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
+  } catch {
+    return (req, res, next) =>
+      next(createHttpError(500, "Can't load swagger docs"));
+  }
+};
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { env } from './utils/env.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import router from './routers/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -16,6 +17,7 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(cors());
   app.use(cookieParser());
+  app.use('/api-docs', swaggerDocs());
 
   app.use(
     pino({
